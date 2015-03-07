2ac66a064e699a37406c4c5e9c2d417df3b18083
Gary Rector <gary.rector@srpnet.com>
2015-03-06 18:43:51 -0700

    plot 4 ylab update

--- plot4.R
+++ plot4.R
@@ -6,7 +6,7 @@ png(filename="plot4.png",width=480,height=480)
 par(mfcol=c(2,2),mar=c(4,4,2,1),oma=c(0,0,0,0))
 with(hpc,{
     plot(datetime,kWact,type="l"
-         ,xlab="",ylab="Global Active Power (kilowatts)")
+         ,xlab="",ylab="Global Active Power")
     plot(datetime,Wh1,type="l"
          ,xlab="",ylab="Energy sub metering" )
     lines(datetime,Wh2,col="red")
@@ -16,7 +16,7 @@ with(hpc,{
          ,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
     plot(datetime,V,type="l"
          ,xlab="datetime",ylab="Voltage" )
-    plot(datetime,kWreact,type="l"
+    plot(datetime,kWreact,type="l",
          ,xlab="datetime",ylab="Global_reactive_power")    
     })
 dev.off()
