48f3795a8afbd70bc2fc18b1caf8723ecc774a95
Gary Rector <gary.rector@srpnet.com>
2015-03-06 18:25:34 -0700

    plot 4 margin updates

--- plot4.R
+++ plot4.R
@@ -3,7 +3,7 @@ if (!exists("getandprep")) source("getandprep.R")
 getandprep()
 
 png(filename="plot4.png",width=480,height=480)
-par(mfcol=c(2,2),mar=c(4,2,2,1),oma=c(0,0,0,0))
+par(mfcol=c(2,2),mar=c(4,4,2,1),oma=c(0,0,0,0))
 with(hpc,{
     plot(datetime,kWact,type="l"
          ,xlab="",ylab="Global Active Power (kilowatts)")
